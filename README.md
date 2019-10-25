6a370c856b461da0e862c17b5b4a94ee3fd9c457
lihongli <lihognli@u51.com>
2019-10-25 23:14:37 +0800

    新增easyExcel读取Excel的Utils

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 #SimpleJavaTest
 记录一些日常使用到的简单的java方法
+### 20191024
+* LRU的java实现方式
+
+### 20191025
+* EasyExcel读取excel的方式
---
+++ src/main/java/utils/easyexcel/EasyExcelUtils.java
@@ -0,0 +1,53 @@
+package utils.easyexcel;
+
+import com.alibaba.excel.ExcelReader;
+import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.event.AnalysisEventListener;
+import com.alibaba.excel.metadata.BaseRowModel;
+import com.alibaba.excel.metadata.Sheet;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.util.List;
+import org.testng.collections.Lists;
+
+/**
+ * Created by lihongli on 2019/10/25.
+ */
+public class EasyExcelUtils {
+
+    public static <T extends BaseRowModel> List<T> doRead(InputStream inputStream, Class<T> clazz) {
+        MyExcelReadListener<T> readListener = new MyExcelReadListener<>();
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+        ExcelReader excelReader = new ExcelReader(bufferedInputStream, null, readListener);
+        excelReader.read(new Sheet(1, 1, clazz));
+        return readListener.getData();
+    }
+
+    public static class MyExcelReadListener<R extends BaseRowModel> extends AnalysisEventListener<R> {
+
+        private List<R> data = Lists.newArrayList();
+
+        /**
+         * when analysis one row trigger invoke function
+         *
+         * @param object one row data
+         * @param context analysis context
+         */
+        @Override
+        public void invoke(R object, AnalysisContext context) {
+            data.add(object);
+        }
+
+        /**
+         * if have something to do after all  analysis
+         */
+        @Override
+        public void doAfterAllAnalysed(AnalysisContext context) {
+//            throw new RuntimeException("aa");
+        }
+
+        public List<R> getData() {
+            return data;
+        }
+    }
+}
