4c345e865f53fb24bd2e65ae17474ff9ac42a33f
gaishibihu <gaishibihu>
2019-12-12 14:06:35 +0800

    同步队列

---
+++ src/main/java/thread/SynchronousQueueDemo.java
@@ -0,0 +1,41 @@
+package thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
+
+public class SynchronousQueueDemo {
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        SynchronousQueue<Integer> queue = new SynchronousQueue<>(true);
+        Thread thread1 = new Thread(() -> {
+            try {
+                queue.put(1);
+                System.out.println("t1 end");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread1.start();
+        Thread.sleep(2000);
+        Thread thread2 = new Thread(() -> {
+            try {
+                queue.put(2);
+                System.out.println("t2 end");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread2.start();
+        Thread.sleep(10000);
+        Thread thread3 = new Thread(() -> {
+            try {
+                System.out.println(queue.take());
+//                System.out.println(queue.take());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread3.start();
+    }
+}
