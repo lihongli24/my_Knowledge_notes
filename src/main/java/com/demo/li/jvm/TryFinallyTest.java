ecdc16f1df82cc6f14536e254740975ba931d387
lihongli <1115035833@qq.com>
2020-04-29 14:03:02 +0800

    jvm invoker

---
+++ src/main/java/com/demo/li/jvm/TryFinallyTest.java
@@ -0,0 +1,23 @@
+package com.demo.li.jvm;
+
+/**
+ * @author lihongli
+ * create：2020/4/29 1:42 下午
+ */
+public class TryFinallyTest {
+
+    public static int func() {
+        try {
+            return 0;
+        } catch (Exception e){
+            return 1;
+        } finally {
+            return 2;
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println(func());
+    }
+
+}
